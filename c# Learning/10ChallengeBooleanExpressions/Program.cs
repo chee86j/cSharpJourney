256f99c21d4693c6894ea6f7c3f40c9d2f2e084e
jeffreychee <jeffreychee86@gmail.com>
2024-12-26 17:55:27 -0500

    10ChallengeBooleanExpressions

---
+++ c# Learning/10ChallengeBooleanExpressions/Program.cs
@@ -0,0 +1,40 @@
+﻿/*  Given Tasks:
+    1.  If the user is an Admin w/level greater than 55
+            output: "Welcome, Super Admin user."
+    2.  If the user is an Admin w/level less than or equal to 55
+            output: "Welcome, Admin user."
+    3.  If the user is a Manager w/level greater than 20
+            output: "You do not have sufficient privileges."
+    4.  If the user is not an Admin or Manager
+            output: "You do not have sufficient privileges."
+*/
+
+string permission = "Admin|Manager";
+int level = 53;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
